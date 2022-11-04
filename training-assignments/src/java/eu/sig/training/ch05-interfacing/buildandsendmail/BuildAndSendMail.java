1154b0b730d815b6839ac3c5666eb3751885b7ee
juliavks <116554154+juliavks@users.noreply.github.com>
2022-11-04 13:43:20 +0100

    Update BuildAndSendMail.java
    Refactoring

--- training-assignments/src/java/eu/sig/training/ch05-interfacing/buildandsendmail/BuildAndSendMail.java
+++ training-assignments/src/java/eu/sig/training/ch05-interfacing/buildandsendmail/BuildAndSendMail.java
@@ -1,39 +1,60 @@
-package eu.sig.training.ch05.buildandsendmail;
+package eu.sig.training.ch05.buildandsendmail.v2;
 
+@SuppressWarnings("unused")
 public class BuildAndSendMail {
+
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
-        String message2, String message3) {
-        // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
-        // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,
-            message1 + message2 +  message3);
-        // Send message
-        m.send(mId, subject, mMessage);
+    public void buildAndSendMail(MailMan m, MailAddress mAddress,
+        MailBody mBody) {
+        // Build the mail
+        Mail mail = new Mail(mAddress, mBody);
+        // Send the mail
+        m.sendMail(mail);
+    }
+
+    private class Mail {
+        private MailAddress address;
+        private MailBody body;
+
+        private Mail(MailAddress mAddress, MailBody mBody) {
+            this.address = mAddress;
+            this.body = mBody;
+        }
+    }
+
+    private class MailBody {
+        String subject;
+        MailMessage message;
+
+        public MailBody(String subject, MailMessage message) {
+            this.subject = subject;
+            this.message = message;
+        }
+    }
+
+    private class MailAddress {
+        private String mId;
+
+        private MailAddress(String firstName, String lastName,
+            String division) {
+            this.mId = firstName.charAt(0) + "." + lastName.substring(0, 7)
+                + "@"
+                + division.substring(0, 5) + ".compa.ny";
+        }
     }
     // end::buildAndSendMail[]
 
-    @SuppressWarnings("unused")
     private MailMessage formatMessage(MailFont font, String string) {
         return null;
     }
 
     private class MailMan {
-
-        @SuppressWarnings("unused")
         public void send(String mId, String subject, MailMessage mMessage) {}
-
+        public void sendMail(Mail mail) {}
     }
 
-    private class MailFont {
+    private class MailFont {}
 
-    }
-
-    private class MailMessage {
-
-    }
+    private class MailMessage {}
 
 }
