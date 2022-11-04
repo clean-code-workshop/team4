befa2bd21436aa1e2a6bc704e806dd155d303f3f
juliavks <116554154+juliavks@users.noreply.github.com>
2022-11-04 13:09:27 +0100

    Update BuildAndSendMail.java
    Improvement 1

--- training-assignments/src/java/eu/sig/training/ch05-interfacing/buildandsendmail/BuildAndSendMail.java
+++ training-assignments/src/java/eu/sig/training/ch05-interfacing/buildandsendmail/BuildAndSendMail.java
@@ -4,13 +4,13 @@ public class BuildAndSendMail {
     // tag::buildAndSendMail[]
     public void buildAndSendMail(MailMan m, String firstName, String lastName,
         String division, String subject, MailFont font, String message1,
-        String message2) {
+        String message2, String message3) {
         // Format the email address
         String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
             + division.substring(0, 5) + ".compa.ny";
         // Format the message given the content type and raw message
         MailMessage mMessage = formatMessage(font,
-            message1 + message2);
+            message1 + message2 + message3);
         // Send message
         m.send(mId, subject, mMessage);
     }
