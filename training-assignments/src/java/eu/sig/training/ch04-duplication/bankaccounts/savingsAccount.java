600cbf6d85de47237cfc908c0979e849147586a7
3rats <jannes.burger@rijksoverheid.nl>
2024-06-28 13:37:03 +0200

    Create savingsAccount.java

---
+++ training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/savingsAccount.java
@@ -0,0 +1,36 @@
+package eu.sig.training.ch04;
+
+// tag::SavingsAccount[]
+public class SavingsAccount {
+    CheckingAccount registeredCounterAccount;
+
+    public Transfer makeTransfer(String counterAccount, Money amount) 
+        throws BusinessException {
+        // 1. Assuming result is 9-digit bank account number,
+        // validate with 11-test:
+        if (Accounts.isValid(counterAccount)) { // <1>
+            // 2. Look up counter account and make transfer object:
+            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+            Transfer result = new Transfer(this, acct, amount); // <2>
+            if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
+            {
+                return result;
+            } else {
+                throw new BusinessException("Counter-account not registered!");
+            }
+        } else {
+            throw new BusinessException("Invalid account number!!");
+        }
+    }
+
+    public void addInterest() {
+        Money interest = balance.multiply(INTEREST_PERCENTAGE);
+        if (interest.greaterThan(0)) {
+            balance.add(interest);
+        } else {
+            balance.substract(interest);
+        }
+    }
+
+}
+// end::SavingsAccount[]
