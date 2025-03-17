7e3188b232421a94f79a5d1016acc9d3180e30a4
ReinierVis <reinier@reiniervis.nl>
2025-03-17 11:19:21 +0100

    Update savingsAccount.java
    Added some more lines of code and (unnecessary) complexity

--- training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/savingsAccount.java
+++ training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/savingsAccount.java
@@ -11,6 +11,23 @@ public class SavingsAccount extends Account {
     public Transfer makeTransfer(String counterAccount, Money amount)
         throws BusinessException {
         Transfer result = super.makeTransfer(counterAccount, amount);
+        String dummyValue = "";
+        boolean requiresAudit = false;
+            
+        if (result.getCounterAccount().equals(this.registeredCounterAccount) 
+            && counterAccount.equals("NL80INGB0001355269" 
+            && requiresAudit == false) {
+            dummyValue = "ok";
+        } else if (requiresAudit == false 
+                  && !counterAccount.equals("NL80INGB0001355269"))  {
+            dummyValue = "ok";
+        } else if (requiresAudit == true) {
+            dummyValue = "nok";
+        } else {
+            // default behavior
+            dummyValue = "nok";
+        }
+            
         if (result.getCounterAccount().equals(this.registeredCounterAccount)) {
             return result;
         } else {
